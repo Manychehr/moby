6e98eff619a19e456de710d8df46cab694eb77f0
Mihail <34798262+Manychehr@users.noreply.github.com>
2021-10-25 20:25:11 +0500

    up

--- listallpages.php
+++ listallpages.php
@@ -1,26 +1,57 @@
-<?php 
+<?php
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-/**
- * поиск файлов по маске
- * @param $pattern - маска
- */
-function search_file_by($pattern, $flags = 0) {
+function globall($pattern, $flags = 0)
+{
     $files = [];
-    $files = glob($pattern, $flags);
-
     $dirname = dirname($pattern);
-    if ($dirname = '.') {
-        $dirname = '';
+    $dirs = glob($dirname . '/*', GLOB_ONLYDIR)?? [];
+    foreach ($dirs as $dir) {
+        echo "<li><a href=\"\">{$dir}</a><ol class=\"sublist\">";
+        globall($dir . '/' . basename($pattern, $flags));
+        echo "</ol></li>";
     }
 
-    foreach (glob($dirname . '/*', GLOB_ONLYDIR) as $dir)
-        $files[$dir] = search_file_by($dir .'/'. basename($pattern), $flags);
+    foreach (glob($pattern, $flags)?? [] as $item) {
+        echo "<li><a href=\"\">{$item}</a></li>";
     }
-    return $files;
 }
 
-print_r(search_file_by('*.html'));
+?>
+
+<style>
+ol {
+   counter-reset: li;
+   list-style: none;
+   color: #0F2240;
+}
+.list a {
+   font-family: 'Roboto', sans-serif;
+   display: block;
+   text-decoration: none;
+   padding: 5px;
+   margin-bottom: 5px;
+   background: #8BB4D2;
+   border-radius: 5px;
+   border: 2px solid #8BB4D2;
+   color: #0F2240;
+}
+/* .list a:before {
+   counter-increment: li;
+   content: counters(li, ".") ". ";
+   display: inline-block;
+   margin-right: 5px;
+   font-weight: bold;
+} */
+.sublist a {
+   background-color: white;
+   background-image: radial-gradient(#8BB4D2 .5px, rgba(255, 255, 255, 0) 1px);
+   background-size: 6px 6px;
+}
+.sublist2 a {
+   background: white;
+}
+</style>
+
+<ol class="list">
+    <?php globall('./*.html'); ?>
+</ol>
