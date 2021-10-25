c70aca8bbe3bbf9086b39a932e102f19a577512e
Mihail <34798262+Manychehr@users.noreply.github.com>
2021-10-25 18:17:19 +0500

    up

--- listallpages.php
+++ listallpages.php
@@ -1,3 +1,18 @@
 <?php 
+/**
+ * поиск файлов по маске
+ * @param $pattern - маска
+ */
+function search_file_by($pattern, $flags = 0) {
+   
+    // поиск по маске в папке
+    $files = glob($pattern, $flags);
+    foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR | GLOB_NOSORT) as $dir)
+       
+        // поиск в подпапках
+        $files = array_merge($files, search_file_by($dir .'/'. basename($pattern), $flags));
+    }
+    return $files;
+}
 
-print_r('test pull');
+print_r(search_file_by('*.html'));
